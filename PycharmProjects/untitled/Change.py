91a5ed46e28b7a5372e57a28aab3cd7571f78de1
jacquie <jc15@email.sc.edu>
2016-02-10 16:35:27 -0500

    made program to determine the amount of change due - needs work deleting the decimals

---
+++ PycharmProjects/untitled/Change.py
@@ -0,0 +1,13 @@
+cents = raw_input("Enter a whole number from 1 to 99.\nThe machine will"
+                  " determine a combination of coins.\n")
+print cents, " cents in coins:"
+cents = float(cents)
+numQ = cents/25
+cents = cents % 25
+numD = cents/10
+cents = cents%10
+numN = cents/5
+cents = cents%5
+numP = cents
+print numQ, " quarters\n", numD, " dimes\n", numN, "nickels\n", numP, \
+    " pennies"
